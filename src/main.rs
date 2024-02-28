760d42948c6ad3ec2eabab2db97e0ee86b26354c
aligator <me@aligator.dev>
2024-02-28 21:39:22 +0100

    Improve matrix code based on some code of github.com/dtcristo/bevy_pixels and implement the bounce example from bevy_pixels

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,11 @@ use clap::Parser;
 use rand::random;
 
 use crate::matrix::{FrameBuffer, HEIGHT, MatrixPlugin, WIDTH};
+use crate::schedule::Draw;
 
 mod matrix;
 mod serial;
+mod schedule;
 
 #[derive(Parser, Debug)]
 #[command(version, arg_required_else_help = true)]
@@ -27,18 +29,122 @@ pub struct ClapCli {
     wait_for_device: bool,
 }
 
-#[derive(Resource)]
-struct SpawnTimer(Timer);
+#[derive(Component, Debug)]
+struct Position {
+    x: u8,
+    y: u8,
+}
 
-fn set_random_pixel(time: Res<Time>, mut timer: ResMut<SpawnTimer>, mut frame_buffer: ResMut<FrameBuffer>) {
-    // update our timer with the time elapsed since the last update
-    // if that caused the timer to finish, we say hello to everyone
-    if timer.0.tick(time.delta()).just_finished() {
-        let x = random::<usize>() % WIDTH;
-        let y = random::<usize>() % HEIGHT;
-        frame_buffer.set_pixel(x, y, 0xff);
+#[derive(Component, Debug)]
+struct Velocity {
+    x: i16,
+    y: i16,
+}
+
+#[derive(Component, Debug)]
+struct Size {
+    width: u8,
+    height: u8,
+}
+
+#[derive(Component, Debug)]
+struct Color(u8);
+
+
+#[derive(Bundle, Debug)]
+struct ObjectBundle {
+    position: Position,
+    velocity: Velocity,
+    size: Size,
+    color: Color,
+}
+
+
+/// Spawn object.
+fn setup(mut commands: Commands) {
+    let box_object = ObjectBundle {
+        position: Position { x: 1, y: 1 },
+        velocity: Velocity { x: 1, y: 1 },
+        size: Size {
+            width: 2,
+            height: 3,
+        },
+        color: Color(0xff),
+    };
+    commands.spawn(box_object);
+}
+
+/// Bounce object off edges of buffer.
+fn bounce(
+    mut query: Query<(&Position, &mut Velocity, &Size, &mut Color)>,
+) {
+    for (position, mut velocity, size, mut color) in &mut query {
+        let mut bounce = false;
+        if position.x == 0 && velocity.x < 0 {
+            velocity.x *= -1;
+            bounce = true;
+        }
+        if position.x + size.width == WIDTH && velocity.x > 0 {
+            velocity.x *= -1;
+            bounce = true;
+        }
+        if position.y == 0 && velocity.y < 0 {
+            velocity.y *= -1;
+            bounce = true;
+        }
+        if position.y + size.height == HEIGHT && velocity.y > 0 {
+            velocity.y *= -1;
+            bounce = true;
+        }
+        if bounce {
+            color.0 = random();
+        }
     }
 }
+
+/// Move object based on current velocity.
+fn movement(time: Res<Time>, mut timer: ResMut<AnimationTimer>,
+            mut query: Query<(&mut Position, &Velocity, &Size)>,
+) {
+    if timer.0.tick(time.delta()).just_finished() {
+        for (mut position, velocity, size) in &mut query {
+            position.x = ((position.x as i16 + velocity.x) as u8).clamp(0, WIDTH - size.width);
+            position.y =
+                ((position.y as i16 + velocity.y) as u8).clamp(0, HEIGHT - size.height);
+        }
+    }
+}
+
+/// Draw solid background to buffer.
+fn draw_background(mut frame_buffer: ResMut<FrameBuffer>) {
+    let length = frame_buffer.0.len();
+    frame_buffer.0.copy_from_slice(&[0x11].repeat(length));
+}
+
+/// Draw objects to buffer.
+fn draw_objects(
+    mut frame_buffer: ResMut<FrameBuffer>,
+    query: Query<(&Position, &Size, &Color)>,
+) {
+    for (position, size, color) in &query {
+        // frame_buffer.set_pixel(2, 33, 0xff)
+        //frame_buffer.set_pixel(position.x, position.y, color.0)
+        let y_offset = position.y as u16;
+        let height_bytes = size.height as u16;
+        let object_col = &[color.0].repeat(size.height as usize);
+
+        for x in position.x as u16..(position.x as u16 + size.width as u16) {
+            let x_offset = x * HEIGHT as u16;
+            let i = x_offset + y_offset;
+            let j = i + height_bytes;
+
+            frame_buffer.0[i as usize..j as usize].copy_from_slice(object_col);
+        }
+    }
+}
+
+#[derive(Resource)]
+struct AnimationTimer(Timer);
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
@@ -46,8 +152,13 @@ fn main() {
 
     App::new()
         .add_plugins(MinimalPlugins)
-        .add_plugins(MatrixPlugin{ list: args.list, verbose: args.verbose, serial_dev: args.serial_dev, wait_for_device: args.wait_for_device })
-        .insert_resource(SpawnTimer(Timer::from_seconds(0.1, TimerMode::Repeating)))
-        .add_systems(Update, set_random_pixel)
+        .add_plugins(MatrixPlugin { list: args.list, verbose: args.verbose, serial_dev: args.serial_dev, wait_for_device: args.wait_for_device })
+        .add_systems(Startup, setup)
+        .add_systems(
+            Update,
+            (bounce, movement).chain(),
+        )
+        .insert_resource(AnimationTimer(Timer::from_seconds(0.2, TimerMode::Repeating)))
+        .add_systems(Draw, (draw_background, draw_objects).chain())
         .run();
 }
--- src/matrix.rs
+++ src/matrix.rs
@@ -1,14 +1,16 @@
 use std::sync::{Arc, Mutex};
 use std::thread;
 
-use bevy::app::FixedMain;
+use bevy::app::MainScheduleOrder;
+use bevy::ecs::schedule::ExecutorKind;
 use bevy::prelude::*;
 use serialport::SerialPort;
 
+use crate::schedule::{Draw, Render};
 use crate::serial::{Command, connect, simple_cmd};
 
-pub const WIDTH: usize = 9;
-pub const HEIGHT: usize = 34;
+pub const WIDTH: u8 = 9;
+pub const HEIGHT: u8 = 34;
 
 struct LightMatrix {
     port: Arc<Mutex<Box<dyn SerialPort>>>,
@@ -27,12 +29,12 @@ impl LightMatrix {
         let _ = thread::spawn(move || -> () {
             if let Ok(port) = &mut port.try_lock() {
                 for x in 0..WIDTH {
-                    let mut col: [u8; HEIGHT + 1] = [0x00; HEIGHT + 1];
+                    let mut col: [u8; HEIGHT as usize + 1] = [0x00; HEIGHT as usize + 1];
                     // Set column number.
                     col[0] = u8::try_from(x).unwrap();
 
                     // Set column pixels.
-                    col[1..].copy_from_slice(&frame_buffer.0[x * HEIGHT..(x + 1) * HEIGHT]);
+                    col[1..].copy_from_slice(&frame_buffer.0[(x as usize * HEIGHT as usize)..((x + 1) as usize * HEIGHT as usize)]);
 
                     // Send column.
                     simple_cmd(port, Command::SendCol, &col, true);
@@ -48,22 +50,22 @@ impl LightMatrix {
 }
 
 #[derive(Resource)]
-pub struct FrameBuffer(pub [u8; WIDTH * HEIGHT]);
+pub struct FrameBuffer(pub [u8; WIDTH as usize * HEIGHT as usize]);
 
 impl Default for FrameBuffer {
     fn default() -> Self {
-        Self([0x00; WIDTH * HEIGHT])
+        Self([0x00; WIDTH as usize * HEIGHT as usize])
     }
 }
 
 impl FrameBuffer {
-    pub fn set_pixel(&mut self, x: usize, y: usize, value: u8) {
-        self.0[y * WIDTH + x] = value;
+    pub fn set_pixel(&mut self, x: u8, y: u8, value: u8) {
+        self.0[x as usize * HEIGHT as usize + y as usize] = value;
     }
 }
 
 
-fn draw_frame(mut light_matrix: NonSendMut<LightMatrix>, frame_buffer: Res<FrameBuffer>) {
+fn render(mut light_matrix: NonSendMut<LightMatrix>, frame_buffer: Res<FrameBuffer>) {
     let buffer_copy = FrameBuffer(frame_buffer.0.clone());
     light_matrix.draw(buffer_copy);
 }
@@ -77,8 +79,21 @@ pub struct MatrixPlugin {
 
 impl Plugin for MatrixPlugin {
     fn build(&self, app: &mut App) {
+        let mut draw_schedule = Schedule::new(Draw);
+        draw_schedule.set_executor_kind(ExecutorKind::SingleThreaded);
+
+        let mut render_schedule = Schedule::new(Render);
+        render_schedule.set_executor_kind(ExecutorKind::SingleThreaded);
+        render_schedule.add_systems(render);
+
         app.insert_non_send_resource(LightMatrix::new(self.list, self.verbose, self.serial_dev.clone(), self.wait_for_device))
             .insert_resource(FrameBuffer::default())
-            .add_systems(FixedMain, draw_frame);
+            .add_schedule(draw_schedule)
+            .add_schedule(render_schedule);
+
+        // Ensure `Draw` and `Render` schedules execute at the correct moment.
+        let mut order = app.world.resource_mut::<MainScheduleOrder>();
+        order.insert_after(PostUpdate, Draw);
+        order.insert_after(Draw, Render);
     }
 }
